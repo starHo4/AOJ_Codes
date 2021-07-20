67d95a8f755ec9e26decfc8daee17f06e9f7e7ba
starHo4 <hi6yukiho4@gmail.com>
2021-07-20 14:04:50 +0900

    Confirm the correct behavior (AC)

---
+++ Courses/ITP1/topic_5/5_D/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void call(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0)
+        {
+            cout << " " << i;
+            continue;
+        }
+        int x = i;
+        while (x > 0)
+        {
+            if (x % 10 == 3)
+            {
+                cout << " " << i;
+                break;
+            }
+            x /= 10;
+        }
+    }
+    cout << endl;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    call(n);
+}
