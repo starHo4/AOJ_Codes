e0bac20b215fa541b6cae6de59a84affa702165d
starHo4 <hi6yukiho4@gmail.com>
2021-07-21 17:01:29 +0900

    Not yet...

---
+++ Challenges/ICPC/Prelim/2017/1616/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 50000; i++)
+    {
+        int n, m;
+        cin >> n >> m;
+        if (n == 0 && m == 0)
+        {
+            break;
+        }
+        for(int j=0; j<n; j++){
+            
+        }
+    }
+}
