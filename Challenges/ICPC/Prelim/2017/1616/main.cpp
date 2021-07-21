c37b9faaf384aa79d98f84fde79191bce0161a7a
starHo4 <hi6yukiho4@gmail.com>
2021-07-21 22:15:08 +0900

    Confirm the correct behavior (AC)

--- Challenges/ICPC/Prelim/2017/1616/main.cpp
+++ Challenges/ICPC/Prelim/2017/1616/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 int main()
 {
@@ -10,8 +12,29 @@ int main()
         {
             break;
         }
-        for(int j=0; j<n; j++){
-            
+        vector<int> a(n);
+        int ans = -1;
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[j];
+        }
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (a[j] + a[k] <= m)
+                {
+                    ans = max(ans, a[j] + a[k]);
+                }
+            }
+        }
+        if (ans == -1)
+        {
+            cout << "NONE" << endl;
+        }
+        else
+        {
+            cout << ans << endl;
         }
     }
 }
