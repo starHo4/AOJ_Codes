33726ee11a077041616668adad5b3172a9166bd7
starHo4 <hi6yukiho4@gmail.com>
2021-07-29 19:46:11 +0900

    Not yet ...

---
+++ Courses/ALDS1/topic_3/3_C/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct LinkedList{
+    vector<int> tbl;
+    LinkedList(){
+    }
+
+};
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        
+    }
+}
