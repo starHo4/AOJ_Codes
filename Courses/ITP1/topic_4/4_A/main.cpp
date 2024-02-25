dcceb61171c745abda0e644876c45df1b5c72265
starHo4 <hi6yukiho4@gmail.com>
2024-02-25 17:42:10 +0900

    20240225

--- Courses/ITP1/topic_4/4_A/main.cpp
+++ Courses/ITP1/topic_4/4_A/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <iomanip>
-using namespace std; 
-int main(){
+using namespace std;
+
+int main()
+{
     int a, b;
     cin >> a >> b;
+    
     int d, r;
     double f;
+    
     d = a / b;
     r = a % b;
-    f = 1.0 * a / b;
-    cout << fixed;
-    cout << setprecision(5);
-    cout << d << " " << r << " " << f << endl;
+    f = (double)a / (double)b;
+    
+    cout << d << " " << r;
+    
+    cout << fixed << setprecision(5);
+    cout << " " << f << endl;
 }
